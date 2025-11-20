324d23a87050b5b3b2df4e45fecf7667ec6fe056
Robert Hurra <robert@hurra.family>
2025-11-20 23:29:03 +0100

    feat: implement t-hero component with slots for dynamic content and styling

--- components/hero.js
+++ components/hero.js
@@ -1,11 +1,34 @@
+const template = document.createElement("template");
+template.innerHTML = `
+    <section class="hero-content">
+
+        <h1 part="hero-title">
+            <slot name="hero-title"></slot>
+        </h1>
+
+        <h2 part="hero-heading">
+            <slot name="hero-heading"></slot>
+        </h2>
+            
+        <div part="hero-body-wrapper">
+            <slot name="hero-body"></slot> 
+        </div>
+        
+        <div part="cta-buttons">
+            <slot name="hero-action" part="cta-button"></slot> 
+        </div>
+
+    </section>
+    `
 class THero extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: "open" });
+        shadow.append(template.content.cloneNode(true));
+    }
+
     connectedCallback() {
-        this.innerHTML = `
-        <section class="hero">
-            <h1>Welcome to hurra.family</h1>
-            <p>Your go-to place for family updates, stories, and more!</p>
-        </section>
-        `;
+        this.innerHTML = `<link rel="stylesheet" href="/components/hero.css">` + this.innerHTML;
     }
 }
 customElements.define("t-hero", THero);
